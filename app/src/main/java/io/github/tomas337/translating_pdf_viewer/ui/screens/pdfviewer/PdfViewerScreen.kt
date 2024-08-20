4cfaf347db2d8c283847dcc2be805f6554b8a7b1
Tomas3371~ <Tomasdavid337@seznam.cz>
2024-08-20 10:41:05 +0200

    feat: added padding, spacing and background color to pager

--- app/src/main/java/io/github/tomas337/translating_pdf_viewer/ui/screens/pdfviewer/PdfViewerScreen.kt
+++ app/src/main/java/io/github/tomas337/translating_pdf_viewer/ui/screens/pdfviewer/PdfViewerScreen.kt
@@ -1,19 +1,18 @@
 package io.github.tomas337.translating_pdf_viewer.ui.screens.pdfviewer
 
-import android.graphics.Bitmap
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,24 +21,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.google.gson.internal.LinkedTreeMap
 import io.github.tomas337.translating_pdf_viewer.domain.model.FileModel
 import io.github.tomas337.translating_pdf_viewer.ui.screens.pdfviewer.viewmodel.PdfViewerViewModel
-import io.github.tomas337.translating_pdf_viewer.utils.Image
 import io.github.tomas337.translating_pdf_viewer.utils.PageContent
-import io.github.tomas337.translating_pdf_viewer.utils.TextBlock
-import kotlin.math.max
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -75,6 +65,7 @@ fun PdfViewerScreen(
                 .fillMaxSize(),
             state = pagerState,
             beyondBoundsPageCount = 1,
+            pageSpacing = 30.dp,
             userScrollEnabled = isScrollable
         ) { pageIndex ->
             val pageContent: List<PageContent> by pdfViewerViewModel
@@ -87,6 +78,8 @@ fun PdfViewerScreen(
                 state = lazyColumnState,
                 modifier = Modifier
                     .fillMaxSize()
+                    .background(Color.White)
+                    .padding(20.dp)
                     .pointerInput(Unit) {
                         detectAndHandleScroll(
                             lazyColumnState = lazyColumnState,
