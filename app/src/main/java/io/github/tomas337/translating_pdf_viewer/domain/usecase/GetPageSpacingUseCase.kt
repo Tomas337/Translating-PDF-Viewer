17864f9d33bd79c4d38384a2bfc2e77ce98f9e06
Tomas3371~ <Tomasdavid337@seznam.cz>
2024-09-10 18:51:36 +0200

    feat: implemented getters

---
+++ app/src/main/java/io/github/tomas337/translating_pdf_viewer/domain/usecase/GetPageSpacingUseCase.kt
@@ -0,0 +1,12 @@
+package io.github.tomas337.translating_pdf_viewer.domain.usecase
+
+import io.github.tomas337.translating_pdf_viewer.data.repository.preferences.PreferencesRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetPageSpacingUseCase(
+    private val preferencesRepository: PreferencesRepository
+) {
+    operator fun invoke(): Flow<Int> {
+        return preferencesRepository.readPageSpacing()
+    }
+}
