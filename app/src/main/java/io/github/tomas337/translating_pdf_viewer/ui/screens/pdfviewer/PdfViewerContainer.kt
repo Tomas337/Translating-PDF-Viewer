dce892dc1f8b92278a0122de2d17bdd9b0a4a1b4
Tomas3371~ <Tomasdavid337@seznam.cz>
2024-09-17 17:39:19 +0200

    feat: hide bottom sheet on tap

--- app/src/main/java/io/github/tomas337/translating_pdf_viewer/ui/screens/pdfviewer/PdfViewerContainer.kt
+++ app/src/main/java/io/github/tomas337/translating_pdf_viewer/ui/screens/pdfviewer/PdfViewerContainer.kt
@@ -1,29 +1,27 @@
 package io.github.tomas337.translating_pdf_viewer.ui.screens.pdfviewer
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.BoxWithConstraintsScope
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.changedToUp
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.Job
@@ -82,10 +80,32 @@ fun PdfViewerContainer(
         },
     ) { innerPadding ->
         if (isInitialized) {
-
-            // TODO implementation: if bottom sheet isn't hidden, use pointerInput to hide sheet on tap outside of the sheet
             BoxWithConstraints(
-                modifier = Modifier.padding(top = innerPadding.calculateTopPadding())
+                modifier = Modifier
+                    .padding(top = innerPadding.calculateTopPadding())
+                    .pointerInput(scaffoldState.bottomSheetState.isVisible) {
+                        if (scaffoldState.bottomSheetState.isVisible) {
+                            awaitPointerEventScope {
+                                while (true) {
+                                    val down = awaitFirstDown(pass = PointerEventPass.Initial)
+                                    down.consume()
+                                    val up = awaitPointerEvent(pass = PointerEventPass.Initial)
+
+                                    val isPress = up.changes.any { it.changedToUp() }
+                                    val isShort = up.changes.any {
+                                        (it.uptimeMillis - it.previousUptimeMillis) <= 300
+                                    }
+
+                                    if (isPress) {
+                                        up.changes.forEach { it.consume() }
+                                    }
+                                    if (isPress && isShort) {
+                                        setSettingsSheetVisibility(false)
+                                    }
+                                }
+                            }
+                        }
+                    }
             ) {
                 content(this)
             }
