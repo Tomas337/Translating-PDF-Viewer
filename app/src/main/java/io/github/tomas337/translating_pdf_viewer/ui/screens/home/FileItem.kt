ce79efdef930e2787fa67d28deda5967e8f00ba6
Tomas3371~ <Tomasdavid337@seznam.cz>
2024-10-30 14:53:38 +0100

    fix: recompose FileItem on document extraction

--- app/src/main/java/io/github/tomas337/translating_pdf_viewer/ui/screens/home/FileItem.kt
+++ app/src/main/java/io/github/tomas337/translating_pdf_viewer/ui/screens/home/FileItem.kt
@@ -14,6 +14,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,7 +37,9 @@ fun FileItem(
     fileInfo: FileModel,
     navController: NavController,
 ) {
-    val isProcessed by remember { mutableStateOf(fileInfo.intToTextStyleMap.isNotEmpty()) }
+    val isProcessed by remember(fileInfo.intToTextStyleMap) {
+        derivedStateOf { fileInfo.intToTextStyleMap.isNotEmpty() }
+    }
 
     val textSize = 20.sp
     val padding = 10.dp
