e6530a2aa1d89687c71f682524ec7d2105502b7f
Tomas337 <Tomasdavid337@seznam.cz>
2024-11-11 15:53:55 +0100

    feat: unimplemented home screen tests

---
+++ app/src/androidTest/java/io/github/tomas337/translating_pdf_viewer/ui/screens/home/HomeScreenTest.kt
@@ -0,0 +1,37 @@
+package io.github.tomas337.translating_pdf_viewer.ui.screens.home
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class HomeScreenTest {
+
+    @Test
+    fun clickFab_addItemToList() {
+        // TODO: implement
+    }
+
+    @Test
+    fun item_showsProgressBar_thenShowVertMenu() {
+        // TODO: implement
+    }
+
+    @Test
+    fun clickItem_navigateToPdfViewerScreen() {
+        // TODO: implement
+    }
+
+    @Test
+    fun clickVertMenu_showContextMenu() {
+        // TODO: implement
+    }
+
+    @Test
+    fun delete_deleteItemFromList() {
+        // TODO: implement
+    }
+
+    @Test
+    fun renameDialog_saveAndCancel() {
+        // TODO: implement
+    }
+}
