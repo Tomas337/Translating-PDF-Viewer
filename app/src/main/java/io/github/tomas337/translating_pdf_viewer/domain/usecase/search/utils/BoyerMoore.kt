0d023b58799e2ded91022f7ee4c5d57e14d7e6f4
Tomas337 <Tomasdavid337@seznam.cz>
2024-12-17 22:02:29 +0100

    feat: compute bad characters function

--- app/src/main/java/io/github/tomas337/translating_pdf_viewer/domain/usecase/search/utils/BoyerMoore.kt
+++ app/src/main/java/io/github/tomas337/translating_pdf_viewer/domain/usecase/search/utils/BoyerMoore.kt
@@ -1,8 +1,45 @@
 package io.github.tomas337.translating_pdf_viewer.domain.usecase.search.utils
 
+import kotlin.math.max
+
 // TODO: unimplemented
 fun boyerMoore(text: String, pattern: String): List<Pair<Int, Int>> {
-    val highlights = emptyList<Pair<Int, Int>>()
+    val highlights = mutableListOf<Pair<Int, Int>>()
+
+    val n = text.length
+    val m = pattern.length
+    val badCharShift = computeBadCharacterRule(pattern)
+    val goodSuffixShift = computeGoodSuffixRule(pattern)
+    var t = 0
+
+    while (t <= n-m) {
+        var j = m-1
+
+        while (j >= 0 && pattern[j] == text[t+j]) {
+            j--
+        }
+
+        if (j == -1) {
+            highlights.add(Pair(t, t+m-1))
+            t += m
+        } else {
+            val char = text[t+j]
+            val lastOccurrence = badCharShift.getOrDefault(char, -1)
+            t += max(j - lastOccurrence, goodSuffixShift.getOrDefault(j, 1))
+        }
+    }
 
     return highlights
+}
+
+private fun computeBadCharacterRule(pattern: String): HashMap<Char, Int> {
+    val occurrenceMap = hashMapOf<Char, Int>()
+    for (i in pattern.indices) {
+        occurrenceMap[pattern[i]] = i
+    }
+    return occurrenceMap
+}
+
+private fun computeGoodSuffixRule(pattern: String): HashMap<Int, Int> {
+
 }
