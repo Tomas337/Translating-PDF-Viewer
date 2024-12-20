3b60a79e6fd87537feb7084d859a91078d79d4a2
Tomas337 <Tomasdavid337@seznam.cz>
2024-12-20 11:15:37 +0100

    feat: implemented boyer moore sunday algorithm

--- app/src/main/java/io/github/tomas337/translating_pdf_viewer/domain/usecase/search/utils/BoyerMooreSunday.kt
+++ app/src/main/java/io/github/tomas337/translating_pdf_viewer/domain/usecase/search/utils/BoyerMooreSunday.kt
@@ -1,8 +1,45 @@
 package io.github.tomas337.translating_pdf_viewer.domain.usecase.search.utils
 
-// TODO: unimplemented
 fun boyerMooreSunday(text: String, pattern: String): List<Pair<Int, Int>> {
-    val highlights = emptyList<Pair<Int, Int>>()
+    if (text.isEmpty() ||
+        pattern.isEmpty() ||
+        pattern.length > text.length
+    ) {
+        return emptyList()
+    }
+
+    val highlights = mutableListOf<Pair<Int, Int>>()
+
+    val n = text.length
+    val m = pattern.length
+    val badCharShift = computeBadCharacterRule(pattern)
+    var t = 0
+
+    while (t <= n-m) {
+        var j = 0
+
+        while (j < m && text[t+j] == pattern[j]) {
+            j++
+        }
+
+        if (j == m) {
+            highlights.add(Pair(t, t+m-1))
+            t += m
+        } else if (t < n-m) {
+            val nextChar = text[t+m]
+            t += badCharShift.getOrDefault(nextChar, m+1)
+        } else {
+            break
+        }
+    }
 
     return highlights
+}
+
+private fun computeBadCharacterRule(pattern: String): HashMap<Char, Int> {
+    val occurrenceMap = hashMapOf<Char, Int>()
+    for (i in pattern.indices) {
+        occurrenceMap[pattern[i]] = pattern.length - i
+    }
+    return occurrenceMap
 }
